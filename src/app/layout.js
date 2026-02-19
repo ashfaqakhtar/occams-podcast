dfee788fc71943b2b637dc94e4061ecfbf0d313b
Ashfaq Akhtar <ashfaqakhtar@live.com>
2026-02-19 19:19:56 +0530

    Add Lenis smooth scrolling functionality and update dependencies

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import LenisProvider from "@/components/LenisProvider";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -19,11 +20,13 @@ export default function RootLayout({ children }) {
             <body
                 className={`${inter.className} antialiased`}>
                 <link rel="icon" href="/logo/Fevicon.svg" />
-                <Navbar />
+                <LenisProvider>
+                    <Navbar />
 
-                {children}
+                    {children}
 
-                <Footer />
+                    <Footer />
+                </LenisProvider>
             </body>
         </html>
     );
